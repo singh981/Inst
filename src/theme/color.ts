d57ebf3957118ab826faaf0e20db9265971d08f3
Karanbir Singh <singh981@gmail.com>
2024-02-05 15:44:14 -0500

    Feature 2: Added app wide color and fonts

---
+++ src/theme/color.ts
@@ -0,0 +1,8 @@
+const colors = {
+    primary: '#40fde6',
+    white: '#ffffff',
+    grey: '#666666',
+    lightGrey: '#cccccc',
+};
+
+export default colors;
---
+++ src/theme/fonts.ts
@@ -0,0 +1,19 @@
+const size = {
+    xs: 10,
+    sm: 12,
+    default: 14,
+    md: 16,
+    lg: 20,
+    xl: 24,
+    xxl: 30,
+};
+
+const weight = {
+    thin: '400',
+    semi: '600',
+    full: '900',
+    bold: 'bold',
+    normal: 'normal',
+};
+
+export {size, weight};
