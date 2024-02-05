b63f6ffde1df75ae73979d40d24173bc65a7a7ce
Karanbir Singh <singh981@gmail.com>
2024-02-05 16:04:59 -0500

    Feature 3: Add react-native-vector-icons

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Hello, World!</Text>
-    </View>
-  );
-}
+    return (
+        <View style={styles.container}>
+            <Text>Hello, World!</Text>
+            <AntDesign name="pluscircle" size={35}  />
+        </View>
+    );
+};
 
-const styles = StyleSheet.create({ 
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-}) 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
 
 export default App;
