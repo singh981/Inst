cf68099f2ef7aefa57bd31482161693dfca4e9e6
Karanbir Singh <singh981@gmail.com>
2024-02-05 19:51:57 -0500

    Feature 5: Single Instagram Post Screen - UI Only
    Feature 5: Single Instagram Post Screen - UI ONLY

--- src/theme/fonts.ts
+++ src/theme/fonts.ts
@@ -1,16 +1,23 @@
+import {TextStyle} from 'react-native';
+
 const size = {
     xs: 10,
     sm: 12,
     default: 14,
     md: 16,
+    xmd: 18,
     lg: 20,
     xl: 24,
     xxl: 30,
 };
 
-const weight = {
+const weight: {[key: string]: TextStyle['fontWeight']} = {
+    light: '200',
     thin: '400',
+    medium: '500',
+    regular: 'normal',
     semi: '600',
+    heavy: '800',
     full: '900',
     bold: 'bold',
     normal: 'normal',
