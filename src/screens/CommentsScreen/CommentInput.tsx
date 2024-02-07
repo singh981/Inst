1772b924ead6f8fee689a6163842355e3d06fd5b
singh981 <singh981@gmail.com>
2024-02-07 17:44:14 -0500

    Feature 13: Add ability to write comment

---
+++ src/screens/CommentsScreen/CommentInput.tsx
@@ -0,0 +1,51 @@
+import {useState} from 'react';
+import {Button, Image, StyleSheet, TextInput, View} from 'react-native';
+import {size} from '../../theme/fonts';
+
+const CommentInput = ({onPost}: any) => {
+    const [text, setText] = useState('');
+
+    const handlePost = () => {
+        onPost(text);
+        setText('');
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.leftContainer}>
+                <Image
+                    source={{
+                        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/4.jpg',
+                    }}
+                    style={{width: 40, aspectRatio: 1, borderRadius: 25}}
+                />
+                <TextInput
+                    value={text}
+                    onChangeText={setText}
+                    placeholder="Write a comment..."
+                    style={{flex: 1, fontSize: size.md}}
+                    numberOfLines={1}
+                />
+            </View>
+            <Button title="Post" onPress={handlePost} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        width: '100%',
+        padding: 10,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        // backgroundColor: 'yellow',
+    },
+    leftContainer: {
+        flexDirection: 'row',
+        gap: 10,
+        width: '85%',
+    },
+});
+
+export default CommentInput;
--- src/screens/CommentsScreen/CommentsScreen.tsx
+++ src/screens/CommentsScreen/CommentsScreen.tsx
@@ -1,18 +1,67 @@
-
-import { FlatList, Text, View } from 'react-native';
-import comments from '../../assets/data/comments.json';
+import {
+    FlatList,
+    KeyboardAvoidingView,
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native';
+import commentsData from '../../assets/data/comments.json';
 import Comment from '../../components/Comment';
 
-const CommentsScreen = () => {
-    return (
-        <FlatList
-            data={comments}
-            renderItem={({ item }) => <Comment comment={item} showDetailComment/>}
-            contentContainerStyle={{ padding: 5, gap: 15 }}
-            showsVerticalScrollIndicator={false}
+import CommentInput from './CommentInput'; // import the new component
+import {useState} from 'react';
+import {IComment} from '../../types/models';
 
-        />
+const CommentsScreen = () => {
+    const [comments, setComments] = useState<IComment[]>(
+        commentsData as IComment[],
+    );
+
+    const handlePost = (comment: string) => {
+        // Create a new comment object
+        const newComment: IComment = {
+            id: Math.random().toString(),
+            comment,
+            createdAt: new Date().toISOString(),
+            numberOfLikes: 0,
+            user: {
+                id: Math.random().toString(),
+                username: 'Vadim_Savin',
+                avatarUrl:
+                    'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/4.jpg',
+            },
+        };
+
+        setComments([newComment, ...comments]);
+    };
+
+    return (
+        <View>
+            <FlatList
+                data={comments}
+                renderItem={({item}) => (
+                    <Comment comment={item} showDetailComment />
+                )}
+                contentContainerStyle={{padding: 5, gap: 15}}
+                showsVerticalScrollIndicator={false}
+            />
+            <KeyboardAvoidingView
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                style={styles.commentInput}>
+                <CommentInput onPost={handlePost} />
+            </KeyboardAvoidingView>
+        </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    commentInput: {
+        width: '100%',
+    },
+});
 
 export default CommentsScreen;
