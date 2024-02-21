2998a6c4e63eed2504ac2ada69a6f10b28c78211
singh981 <singh981@gmail.com>
2024-02-21 14:21:07 -0500

    Feature 31: (Backend Starts Here) Amplify initialize

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
  */
 
 import {AppRegistry} from 'react-native';
+import {Amplify} from 'aws-amplify';
 import App from './src/App';
 import {name as appName} from './app.json';
+
+import amplifyconfig from './src/amplifyconfiguration.json';
+
+Amplify.configure(amplifyconfig);
 
 AppRegistry.registerComponent(appName, () => App);
