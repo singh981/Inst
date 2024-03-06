f5485351b86bf4c5533cf420e5a9cdb148320656
singh981 <singh981@gmail.com>
2024-03-06 12:57:47 -0500

    Feature 39(1): Added database models (User, Post, Comment, Like) and generated schema (amplify pull)

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "InstagramNotJustDev": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "InstagramNotJustDev": {
       "AppClientID": "string",
